09e8af21ad446e2d565217f35f303223d6ccfa88
Mark <supermark11@gmail.com>
2019-10-09 14:45:39 +0300

    Flyweight is done

--- Flyweight/BacteriaFactory.cs
+++ Flyweight/BacteriaFactory.cs
@@ -10,20 +10,13 @@ namespace Flyweight
     {
         Queue<Bacterium> flyweights = new Queue<Bacterium>();
         private int Index;
-        public BacteriaFactory()
-        {
-            for (Index = 0; Index < 100; Index++)
-            {
-                // create Bacterium with id extrinsicState - form, size; intrinsicState = id of real bacteriem in pool
-                flyweights.Enqueue(new ConcreteBacterium(Index));
-            }
-        }
+
         public Bacterium GetBacteria(int size, string shape)
         {
-            if(flyweights.Count != 0)
+            if (flyweights.Count >   0)
             {
                 ConcreteBacterium bacterium = (ConcreteBacterium)flyweights.Dequeue();
-                bacterium.Explore(size, shape);
+                bacterium.Adjust(size, shape);
                 return bacterium;
             }
             else
@@ -32,6 +25,7 @@ namespace Flyweight
                 return new ConcreteBacterium(Index);
             }
         }
+
         public void ReturnBacteria(Bacterium bacterium)
         {
             flyweights.Enqueue(bacterium);
--- Flyweight/MainWindow.xaml.cs
+++ Flyweight/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,26 +16,103 @@ using System.Windows.Shapes;
 
 namespace Flyweight
 {
-    
-    
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Timer getTimer;
+        public Timer returnTimer;
+        public Timer stopTimer;
         public static ListBox ReportListBox { get; private set; }
+        BacteriaFactory bacteriaFactory;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        private List<string> shapes;
+
+        /*alive bacteria*/
+        Queue<Bacterium> bacteria;
+        
+        /*Report is canceled after session value reaches 20*/
+        int session = 0;
+
         public MainWindow()
         {
             InitializeComponent();
             ReportListBox = ListBox1;
+
+            shapes = new List<string>();
+            shapes.Add("square");
+            shapes.Add("round");
+
+            bacteriaFactory = new BacteriaFactory();
+            bacteria = new Queue<Bacterium>();
+
+            /*first timer*/
+            getTimer = new Timer();
+            getTimer.Interval = 1150;
+            getTimer.Elapsed += GetTimer_Elapsed;
+            getTimer.Enabled = true;
+
+            /*second timer*/
+            returnTimer = new Timer();
+            returnTimer.Interval = 2000;
+            returnTimer.Elapsed += ReturnTimer_Elapsed;
+            returnTimer.Enabled = true;
         }
 
+        private void GetTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (session < 20)
+            {
+                Random random = new Random();
+
+                int size = random.Next(1, 10);
+                int i = random.Next(0, shapes.Count - 1);
+                string shape = shapes[i];
+
+                ConcreteBacterium bacterium = (ConcreteBacterium)bacteriaFactory.GetBacteria(size, shape);
+                bacteria.Enqueue(bacterium);
+
+                stringBuilder.Clear();
+                stringBuilder.AppendFormat("з'явилася нова бактерія із формою: {0}, розміром {1} та індексом {2}", bacterium.Shape, bacterium.Size, bacterium.Id);
+                Dispatcher.Invoke(() =>
+                {
+                    ReportListBox.Items.Add(stringBuilder);
+                });
+                session++;
+            }
+            else
+            {
+                getTimer.Stop();
+                returnTimer.Stop();
+            }
+            
+        }
+
+        private void ReturnTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (bacteria.Count > 0)
+            {
+                ConcreteBacterium bacterium = (ConcreteBacterium)bacteria.Dequeue();
+                bacteriaFactory.ReturnBacteria(bacterium);
+
+                stringBuilder.Clear();
+                stringBuilder.AppendFormat("вмерла бактерія із формою: {0}, розміром {1} та індексом {2}", bacterium.Shape, bacterium.Size, bacterium.Id);
+                Dispatcher.Invoke(() =>
+                {
+                    ReportListBox.Items.Add(stringBuilder);
+                });
+            }
+        }
     }
 
     public abstract class Bacterium
     {
         // Operation - Explore, ExtrinisicStates are size and shape
-        public abstract void Explore(int size, string shape);
+        public abstract void Adjust(int size, string shape);
     }
 
     public class ConcreteBacterium : Bacterium
@@ -47,19 +125,7 @@ namespace Flyweight
         {
             Id = id;
         }
-        public override void Explore(int size, string shape)
-        {
-            Size = size;
-            Shape = shape;
-        }
-    }
-
-    public class UnsharedConcreteBacterium : Bacterium
-    {
-        public int Size { get; private set; }
-        public string Shape { get; private set; }
-
-        public override void Explore(int size, string shape)
+        public override void Adjust(int size, string shape)
         {
             Size = size;
             Shape = shape;
